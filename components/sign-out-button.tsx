ae7ad4ccf61b3b36a1d4c6b572284fa090abf235
Luca Di Gianni <luca.dgni@gmail.com>
2026-02-22 11:31:09 +0100

    refactor: enhance loading state management in sign-in and sign-out buttons

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -1,21 +1,30 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { signOut } from "@/lib/auth/auth-client";
 
 export default function SignOutButton() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   return (
     <Button
       variant="outline"
+      disabled={loading}
       onClick={async () =>
         await signOut({
           fetchOptions: {
             onSuccess: () => {
               router.refresh();
+            },
+            onRequest: () => {
+              setLoading(true);
+            },
+            onResponse: () => {
+              setLoading(false);
             },
           },
         })
