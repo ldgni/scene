792edd8eb3675f9d5c399083e10f09cd3d7fd372
Luca Di Gianni <luca.dgni@gmail.com>
2026-02-23 12:14:36 +0100

    feat: add ErrorPage, Loading, NotFoundPage components and Spinner UI component

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-extrabold tracking-tight">Oops!</h1>
+      <p className="text-muted-foreground text-sm">
+        This page doesn&apos;t exist.
+      </p>
+    </div>
+  );
+}
